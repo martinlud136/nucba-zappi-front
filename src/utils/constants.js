bc71da5717332b6ad63aca1ef997cc7e39938b86
martinlud136 <hectormartinluduena@gmail.com>
2023-08-15 20:50:48 -0300

    constante y conexion a db

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8080';
-export const ADMIN = '50yun4dm1n';
+export const BASE_URL = 'https://clases-nucba-backend.vercel.app';
+export const ADMIN = '50yun4admin';
 export const USER = '50yunu53r';
